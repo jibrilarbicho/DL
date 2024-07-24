f1732893f2e537c7a5b51fe5c3d6815b4d953a2b
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-24 21:59:30 +0300

    Matrix Multiplication using torch

--- matrixMultiplication.py
+++ matrixMultiplication.py
@@ -4,3 +4,7 @@ A=np.random.randn(3,4)
 B=np.random.randn(4,5)
 C=np.random.randn(3,7)
 print(np.round(A@B,2)) #@  for matrix multiplication    
+D=torch.randn(3,4)
+E=torch.randn(4,5)
+F=torch.randn(3,7)
+print(np.round(D@E,2)) #@  for matrix multiplication
