20bd3b4ec56f528460d0142ecd0a1b1474ab6f7d
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-24 14:38:17 +0300

    Vector and Matrix Transpose

--- vectorAndMatrixTranspose.py
+++ vectorAndMatrixTranspose.py
@@ -1,4 +1,13 @@
 import numpy as np
+import torch
 nv=np.array([[1,2,3,4,5]])
 print(nv)
 print(nv.T)
+nvT=nv.T
+print(nvT.T)
+tv=torch.tensor([[1,2,3,4,5]])
+print("torch")
+print(tv)
+print(tv.T)
+tvT = tv.T
+print(tvT.T)
