38f1ba18f003240591f8dbf2d7e627344f5e2484
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-24 15:36:53 +0300

    dot product

---
+++ dotProduct.py
@@ -0,0 +1,11 @@
+import numpy as np
+import torch
+nv1=np.array([1,2,3,4])
+nv2=np.array([0,1,0,-1])
+print(np.dot(nv1,nv2))
+print(np.sum(nv1*nv2))
+
+tv1=torch.tensor([1,2,3,4])
+tv2=torch.tensor([0,1,0,-1])
+print(torch.dot(tv1,tv2))
+print(torch.sum(tv1*tv2))
