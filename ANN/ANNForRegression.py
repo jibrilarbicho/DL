547b993c8e7b8749c9a153db5e8be5414ac01cab
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-08-01 10:25:15 +0300

    ANN

---
+++ ANN/ANNForRegression.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+from IPython import display
+display.set_matplotlib_formats("svg")
+N=30
+x=torch.randn(N,1)
+y=x+torch.randn(N,1)/2
+plt.plot(x,y, "s")
+plt.show()
+ANNreg=nn.Sequential(
+    nn.Linear(1,1),
+    nn.ReLU(),
+    nn.Linear(1,1)
+)
+learningrate=0.05
+lossfunction=nn.MSELoss()
+optimizer=torch.optim.SGD(ANNreg.parameters(), lr=learningrate)
+numepochs=500
+losses=torch.zeros(numepochs, dtype=torch.float)
+for epoch in range(numepochs):
+    yHat=ANNreg(x)
+    loss=lossfunction(yHat,y)
+    losses[epoch]=loss
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+predictions=ANNreg(x)
+
+testloss=(predictions-y).pow(2).mean()
+
+plt.plot(losses.detach(),"o", markerfacecolor="w",linewidth=.1)
+plt.plot(numepochs,testloss.detach(),"ro")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.show()
