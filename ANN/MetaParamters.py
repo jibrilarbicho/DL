807525a5ee1a0dddab6458feff65f751fbae28be
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-09-10 12:12:56 +0300

    Slpiting data and batchsizing

--- ANN/MetaParamters.py
+++ ANN/MetaParamters.py
@@ -9,13 +9,38 @@ import seaborn as sns
 url="https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
 data=pd.read_csv(url,sep=";")
 # print(data.describe())
-for i in data.keys():
-    print(f'{i} Ihas {len (np.unique(data[i]))} unique values')
+# for i in data.keys():
+#     print(f'{i} Ihas {len (np.unique(data[i]))} unique values')
+data = data[data['total sulfur dioxide']<200]
+
+# fig,ax = plt.subplots(1, figsize=(17,4))
+# ax = sns.boxplot(data=data)
+# ax.set_xticklabels(ax.get_xticklabels(),rotation=45) 
+# plt.show()
 cols2zscore=data.keys()
 cols2zscore=cols2zscore.drop("quality")
 for col in cols2zscore:
     mean=np.mean(data[col])
     std=np.std(data[col])
     data[col]=(data[col]-mean)/std
-print(data)
-
+fig,ax = plt.subplots(1, figsize=(17,4))
+ax = sns.boxplot(data=data)
+ax.set_xticklabels(ax.get_xticklabels(),rotation=45) 
+plt.show()
+data["boolQuality"]=0
+data["boolQuality"][data["quality"]>5]=1
+print(data[["quality","boolQuality"]])
+dataT=torch.tensor(data[cols2zscore].values).float()
+labelsT=torch.tensor(data["boolQuality"].values).float()
+labelsT=labelsT[:,None]
+print(dataT)
+print(labelsT.shape)
+train_data, test_data, train_labels, test_lables=train_test_split(dataT, labelsT, test_size=0.2)
+train_data=TensorDataset(train_data, train_labels)
+test_data=TensorDataset(test_data, test_lables)
+batchsize=64
+train_loader=DataLoader(train_data, batch_size=batchsize, shuffle=True ,drop_last=True)
+test_loader=DataLoader(test_data, batch_size=test_data.tensors[0].shape[0])
+for X,y in train_loader:
+    print(X.shape,y.shape)
+    
